9fa536d4f9dc635107affef9205c590ebd954e35
dedesaepulloh <dedesaepulloh77@gmail.com>
2019-09-20 14:28:57 +0700

    initial commit

--- index.php
+++ index.php
@@ -7,13 +7,12 @@
     <title>Registrasi Member AIDI</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="js/aos/aos.css">
-        <link rel="stylesheet" type="text/css" href="bootstrap-4.3.1-dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <script type="text/javascript" src="js/jquery.js"></script>
-        <script src="bootstrap-4.3.1-dist/js/bootstrap.js"></script>
+        <script src="js/bootstrap.js"></script>
 </head>
 <body>
-    <!-- <svg class="ssvg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#e74c3c" fill-opacity="1" d="M0,288L60,282.7C120,277,240,267,360,250.7C480,235,600,213,720,218.7C840,224,960,256,1080,250.7C1200,245,1320,203,1380,181.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
-     -->
+     
      <div class="svvg">
         <img src="images/wave.svg">
      </div>
@@ -24,7 +23,6 @@
     </div>
     
         <form method="POST" action="cetak.php" enctype="multipart/form-data"> 
-        
             <table>
                 <tr>
                     <td>Nama</td>
@@ -86,7 +84,7 @@
                 <?php 
                     $fileku = "file.txt";
                     $filehandle = fopen($fileku, 'r') or die ("File gagal dibuka");
-                    $data = fread($filehandle, 1000);
+                    $data = fread($filehandle, 20000);
                     fclose($filehandle);
                     echo "$data";
                 ?>
@@ -96,7 +94,7 @@
         <script type="text/javascript" src="js/particles.js"></script>
         <script type="text/javascript" src="js/app.js"></script>
         <script type="text/javascript" src="js/aos/aos.js"></script>
-        <script src="js/typed.js-master/lib/typed.js"></script>
+        <script src="js/typed.js"></script>
         <script>
             AOS.init();
         </script>
